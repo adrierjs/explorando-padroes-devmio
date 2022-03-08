953af87bc1c9a35d3d6be3d74a6ceb864757ae25
adrier <adrierj@gmail.com>
2022-03-07 23:26:34 -0300

    project

---
+++ explorando-padroes-devmio/src/main/java/one/digitalinnovation/labpadroesprojetojavaspring/Application.java
@@ -0,0 +1,20 @@
+package one.digitalinnovation.labpadroesprojetojavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/*
+
+ * @author adrier
+ */
+
+@EnableFeignClients
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ explorando-padroes-devmio/src/main/java/one/digitalinnovation/labpadroesprojetojavaspring/services/ProductsService.java
@@ -0,0 +1,20 @@
+package one.digitalinnovation.labpadroesprojetojavaspring.services;
+
+import one.digitalinnovation.labpadroesprojetojavaspring.entities.Products;
+
+/*
+
+ * @author adrier
+ */
+public interface ProductsService {
+
+    Iterable<Products> findAll();
+
+    Products findById(Long id);
+
+    void insert(Products product);
+
+    void update(Long id, Products product);
+
+    void delete(Long id);
+}
